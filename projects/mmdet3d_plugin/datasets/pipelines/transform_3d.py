8b6c17a7b69bdac9b96d9eb994a71b98ab017bc6
xuihaipiaoxiang0 <56183476+xuehaipiaoxiang@users.noreply.github.com>
2025-05-23 14:16:20 +0800

    release

--- projects/mmdet3d_plugin/datasets/pipelines/transform_3d.py
+++ projects/mmdet3d_plugin/datasets/pipelines/transform_3d.py
@@ -1828,3 +1828,260 @@ class CustomResizeCropFlipImageTTA(object):
         gt_labels = gt_labels[indices_res]
 
         return bboxes, centers2d, gt_labels, depths
+    @PIPELINES.register_module()
+class MyResizeCropFlipRotImage():
+    def __init__(self, data_aug_conf=None, with_2d=True, filter_invisible=True, training=True, with_seg=True):
+        self.data_aug_conf = data_aug_conf
+        self.training = training
+        self.min_size = 2.0
+        self.with_2d = with_2d
+        self.filter_invisible = filter_invisible
+        self.with_seg = with_seg
+
+    def point_label_transform(self, point_label, resize, resize_dims, crop, flip, rotate):
+        H, W = resize_dims
+        point_label[:, :2] = point_label[:, :2] * resize
+        point_label[:, 0] -= crop[0]
+        point_label[:, 1] -= crop[1]
+        if flip:
+            point_label[:, 0] = resize_dims[1] - point_label[:, 0]
+
+        point_label[:, 0] -= W / 2.0
+        point_label[:, 1] -= H / 2.0
+
+        h = rotate / 180 * np.pi
+        rot_matrix = [
+            [np.cos(h), np.sin(h)],
+            [-np.sin(h), np.cos(h)],
+        ]
+        point_label[:, :2] = np.matmul(rot_matrix, point_label[:, :2].T).T
+
+        point_label[:, 0] += W / 2.0
+        point_label[:, 1] += H / 2.0
+
+        coords = point_label[:, :2].astype(np.int16)
+
+        depth_map = np.zeros(resize_dims)
+        valid_mask = ((coords[:, 1] < resize_dims[0])
+                    & (coords[:, 0] < resize_dims[1])
+                    & (coords[:, 1] >= 0)
+                    & (coords[:, 0] >= 0))
+        depth_map[coords[valid_mask, 1],
+                coords[valid_mask, 0]] = point_label[valid_mask, 2]
+        semantic_map = np.zeros(resize_dims)
+        semantic_map[coords[valid_mask, 1],
+                coords[valid_mask, 0]] = (point_label[valid_mask, 3] >= 0)
+        return torch.Tensor(depth_map), torch.Tensor(semantic_map)
+
+    def __call__(self, results):
+
+        imgs = results['img']
+        N = len(imgs)
+        new_imgs = []
+        new_gt_bboxes = []
+        new_centers2d = []
+        new_gt_labels = []
+        new_depths = []
+
+        new_masks = []
+        new_gt_depth = []
+
+        masks = results['gt_fg']
+        point_labels  = results['gt_depth']
+
+        assert self.data_aug_conf['rot_lim'] == (0.0, 0.0), "Rotation is not currently supported"
+
+        resize, resize_dims, crop, flip, rotate = self._sample_augmentation()
+
+
+        for i in range(N):
+            img = Image.fromarray(np.uint8(imgs[i]))
+            mask = masks[i]
+            point_label = point_labels[i]
+            img, mask, ida_mat = self._img_transform(
+                img,
+                mask,
+                resize=resize,
+                resize_dims=resize_dims,
+                crop=crop,
+                flip=flip,
+                rotate=rotate,
+            )
+            mask = torch.Tensor((np.array(mask)>0.25))
+            new_masks.append(mask)
+
+            point_depth_augmented, _ = \
+                    self.point_label_transform(
+                            point_label, resize, self.data_aug_conf['final_dim'],
+                            crop, flip, rotate)
+            point_depth_augmented = point_depth_augmented
+            new_gt_depth.append(point_depth_augmented)
+
+            if self.training and self.with_2d: # sync_2d bbox labels
+                gt_bboxes = results['gt_bboxes'][i]
+                centers2d = results['centers2d'][i]
+                gt_labels = results['gt_labels'][i]
+                depths = results['depths'][i]
+                if len(gt_bboxes) != 0:
+                    gt_bboxes, centers2d, gt_labels, depths = self._bboxes_transform(
+                        gt_bboxes, 
+                        centers2d,
+                        gt_labels,
+                        depths,
+                        resize=resize,
+                        crop=crop,
+                        flip=flip,
+                    )
+                if len(gt_bboxes) != 0 and self.filter_invisible:
+                    gt_bboxes, centers2d, gt_labels, depths =  self._filter_invisible(gt_bboxes, centers2d, gt_labels, depths)
+
+                new_gt_bboxes.append(gt_bboxes)
+                new_centers2d.append(centers2d)
+                new_gt_labels.append(gt_labels)
+                new_depths.append(depths)
+
+            new_imgs.append(np.array(img).astype(np.float32))
+            results['intrinsics'][i][:3, :3] = ida_mat @ results['intrinsics'][i][:3, :3]
+        results['gt_bboxes'] = new_gt_bboxes
+        results['centers2d'] = new_centers2d
+        results['gt_labels'] = new_gt_labels
+        results['depths'] = new_depths
+        results['img'] = new_imgs
+        results['lidar2img'] = [results['intrinsics'][i] @ results['extrinsics'][i] for i in range(len(results['extrinsics']))]
+
+        results['gt_fg'] = torch.stack(new_masks)
+        results['gt_depth'] = torch.stack(new_gt_depth)
+
+        return results
+
+    def _bboxes_transform(self, bboxes, centers2d, gt_labels, depths,resize, crop, flip):
+        assert len(bboxes) == len(centers2d) == len(gt_labels) == len(depths)
+        fH, fW = self.data_aug_conf["final_dim"]
+        bboxes = bboxes * resize
+        bboxes[:, 0] = bboxes[:, 0] - crop[0]
+        bboxes[:, 1] = bboxes[:, 1] - crop[1]
+        bboxes[:, 2] = bboxes[:, 2] - crop[0]
+        bboxes[:, 3] = bboxes[:, 3] - crop[1]
+        bboxes[:, 0] = np.clip(bboxes[:, 0], 0, fW)
+        bboxes[:, 2] = np.clip(bboxes[:, 2], 0, fW)
+        bboxes[:, 1] = np.clip(bboxes[:, 1], 0, fH) 
+        bboxes[:, 3] = np.clip(bboxes[:, 3], 0, fH)
+        keep = ((bboxes[:, 2] - bboxes[:, 0]) >= self.min_size) & ((bboxes[:, 3] - bboxes[:, 1]) >= self.min_size)
+
+
+        if flip:
+            x0 = bboxes[:, 0].copy()
+            x1 = bboxes[:, 2].copy()
+            bboxes[:, 2] = fW - x0
+            bboxes[:, 0] = fW - x1
+        bboxes = bboxes[keep]
+
+        centers2d  = centers2d * resize
+        centers2d[:, 0] = centers2d[:, 0] - crop[0]
+        centers2d[:, 1] = centers2d[:, 1] - crop[1]
+        centers2d[:, 0] = np.clip(centers2d[:, 0], 0, fW)
+        centers2d[:, 1] = np.clip(centers2d[:, 1], 0, fH) 
+        if flip:
+            centers2d[:, 0] = fW - centers2d[:, 0]
+
+        centers2d = centers2d[keep]
+        gt_labels = gt_labels[keep]
+        depths = depths[keep]
+
+        return bboxes, centers2d, gt_labels, depths
+
+
+    def _filter_invisible(self, bboxes, centers2d, gt_labels, depths):
+        # filter invisible 2d bboxes
+        assert len(bboxes) == len(centers2d) == len(gt_labels) == len(depths)
+        fH, fW = self.data_aug_conf["final_dim"]
+        indices_maps = np.zeros((fH,fW))
+        tmp_bboxes = np.zeros_like(bboxes)
+        tmp_bboxes[:, :2] = np.ceil(bboxes[:, :2])
+        tmp_bboxes[:, 2:] = np.floor(bboxes[:, 2:])
+        tmp_bboxes = tmp_bboxes.astype(np.int64)
+        sort_idx = np.argsort(-depths, axis=0, kind='stable')
+        tmp_bboxes = tmp_bboxes[sort_idx]
+        bboxes = bboxes[sort_idx]
+        depths = depths[sort_idx]
+        centers2d = centers2d[sort_idx]
+        gt_labels = gt_labels[sort_idx]
+        for i in range(bboxes.shape[0]):
+            u1, v1, u2, v2 = tmp_bboxes[i]
+            indices_maps[v1:v2, u1:u2] = i
+        indices_res = np.unique(indices_maps).astype(np.int64)
+        bboxes = bboxes[indices_res]
+        depths = depths[indices_res]
+        centers2d = centers2d[indices_res]
+        gt_labels = gt_labels[indices_res]
+
+        return bboxes, centers2d, gt_labels, depths
+
+
+
+    def _get_rot(self, h):
+        return torch.Tensor(
+            [
+                [np.cos(h), np.sin(h)],
+                [-np.sin(h), np.cos(h)],
+            ]
+        )
+
+    def _img_transform(self, img, mask, resize, resize_dims, crop, flip, rotate):
+        ida_rot = torch.eye(2)
+        ida_tran = torch.zeros(2)
+        # adjust image
+        img = img.resize(resize_dims)
+        img = img.crop(crop)
+        if flip:
+            img = img.transpose(method=Image.FLIP_LEFT_RIGHT)
+        img = img.rotate(rotate)
+
+        mask = mask.resize(resize_dims)
+        mask = mask.crop(crop)
+        if flip:
+            mask = mask.transpose(method=Image.FLIP_LEFT_RIGHT)
+        mask = mask.rotate(rotate)
+
+        # post-homography transformation
+        ida_rot *= resize
+        ida_tran -= torch.Tensor(crop[:2])
+        if flip:
+            A = torch.Tensor([[-1, 0], [0, 1]])
+            b = torch.Tensor([crop[2] - crop[0], 0])
+            ida_rot = A.matmul(ida_rot)
+            ida_tran = A.matmul(ida_tran) + b
+        A = self._get_rot(rotate / 180 * np.pi)
+        b = torch.Tensor([crop[2] - crop[0], crop[3] - crop[1]]) / 2
+        b = A.matmul(-b) + b
+        ida_rot = A.matmul(ida_rot)
+        ida_tran = A.matmul(ida_tran) + b
+        ida_mat = torch.eye(3)
+        ida_mat[:2, :2] = ida_rot
+        ida_mat[:2, 2] = ida_tran
+        return img, mask, ida_mat
+
+    def _sample_augmentation(self):
+        H, W = self.data_aug_conf["H"], self.data_aug_conf["W"]
+        fH, fW = self.data_aug_conf["final_dim"]
+        if self.training:
+            resize = np.random.uniform(*self.data_aug_conf["resize_lim"])
+            resize_dims = (int(W * resize), int(H * resize))
+            newW, newH = resize_dims
+            crop_h = int((1 - np.random.uniform(*self.data_aug_conf["bot_pct_lim"])) * newH) - fH
+            crop_w = int(np.random.uniform(0, max(0, newW - fW)))
+            crop = (crop_w, crop_h, crop_w + fW, crop_h + fH)
+            flip = False
+            if self.data_aug_conf["rand_flip"] and np.random.choice([0, 1]):
+                flip = True
+            rotate = np.random.uniform(*self.data_aug_conf["rot_lim"])
+        else:
+            resize = max(fH / H, fW / W)
+            resize_dims = (int(W * resize), int(H * resize))
+            newW, newH = resize_dims
+            crop_h = int((1 - np.mean(self.data_aug_conf["bot_pct_lim"])) * newH) - fH
+            crop_w = int(max(0, newW - fW) / 2)
+            crop = (crop_w, crop_h, crop_w + fW, crop_h + fH)
+            flip = False
+            rotate = 0
+        return resize, resize_dims, crop, flip, rotate
